b0ad6392020579df137537ea1a78dcc870ae81b8
Jamie <Jamie@144.89.236.40>
2017-11-01 21:18:46 -0500

    read done

--- src/csci335/Reader.java
+++ src/csci335/Reader.java
@@ -2,13 +2,24 @@ package csci335;
 
 public class Reader extends Thread{
 	private String readThread;
+	private final ReadWriteLock lock;
 	
-	Reader(String name){
-		readThread = name; //allow to create instance of thread
+//	Reader(String name){
+//		readThread = name; //allow to create instance of thread
+//	}
+	public Reader(ReadWriteLock lock){
+		//give same version of class to all threads
+		this.lock = lock;
 	}
 	
 	public void run(){
-		
+		try {
+			//calls lock with value 3
+			lock.reader();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	
