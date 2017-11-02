63e96710b611c6791d75b4a5d5c6d7d9823170fb
Jamie <Jamie@144.89.236.40>
2017-11-01 20:09:24 -0500

    writer done

--- src/csci335/Writer.java
+++ src/csci335/Writer.java
@@ -2,5 +2,20 @@ package csci335;
 
 public class Writer extends Thread {
 	private String writeThread;
-
+	//instance of lock class
+	private final ReadWriteLock lock;
+	
+	public Writer(ReadWriteLock lock){
+		//give same version of class to all threads
+		this.lock = lock;
+	}
+	public void run(){
+		try {
+			//calls lock with value 3
+			lock.writer(3);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
